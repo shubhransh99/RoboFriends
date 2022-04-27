9588dad4a953b36ca7fe40e7036cfb86f3bb3cd0
shubhransh99 <shubhransh99@gmail.com>
2022-04-28 04:06:42 +0530

    Updates

---
+++ precache-manifest.f21e771acdf42a996e78e28de2453c2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0973d8b19731a07ace5f",
+    "url": "/RoboFriends/static/css/main.a9201627.chunk.css"
+  },
+  {
+    "revision": "0973d8b19731a07ace5f",
+    "url": "/RoboFriends/static/js/main.0973d8b1.chunk.js"
+  },
+  {
+    "revision": "0ccef95fae844ed36350",
+    "url": "/RoboFriends/static/css/1.71dd98f9.chunk.css"
+  },
+  {
+    "revision": "0ccef95fae844ed36350",
+    "url": "/RoboFriends/static/js/1.0ccef95f.chunk.js"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "6a3079deeb654627a407337f400468fa",
+    "url": "/RoboFriends/index.html"
+  }
+];
